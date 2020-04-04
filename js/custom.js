4cbe4768e59c1266e7c3367fd4908fa4a89c7a74
paki_jan@yahoo.com <paki_jan@yahoo.com>
2020-04-04 09:45:35 +0500

    Letter Counter

---
+++ js/custom.js
@@ -0,0 +1,181 @@
+
+
+// let form = document.getElementById("inputtext");
+
+
+// let itemList = document.getElementById('Items');
+
+
+// form.addEventListener('submit', additem);
+// itemList.addEventListener('click', removeItem);
+
+
+
+
+
+
+     let  h = document.createElement('h1');
+     let  text = document.createTextNode('Yes Right Hello World'); 
+
+       let main = h.appendChild('text');
+     console.log(main);
+     
+
+// const  number = ['One','Two','Three','Four','Five','Six']
+
+//            testNumber.array.forEach(number => {
+//                 console.log(testNumber);
+//            });
+           
+// console.log(familyNames);
+
+
+// function additem(e) {
+//     e.preventDefault();
+//     let newItem = document.getElementById("item").value;
+//     let li = document.createElement("li");
+//     let deleteBtn = document.createElement("button");
+//      deleteBtn.className = 'float-right btn-sm btn-danger delete';
+//     li.className = 'list-group-item';
+//     // console.log(li);
+//     deleteBtn.appendChild(document.createTextNode("Delete Item"));
+//     li.appendChild(deleteBtn);
+//     li.appendChild(document.createTextNode(newItem));
+
+//     itemList.appendChild(li);
+    
+//     ext install TabNine.tabnine-vscode
+
+// }
+// function removeItem(e) {
+//    if (e.target.classList.contains('delete')) {
+//        if (confirm("Are You Sure")) {
+//            let li = e.target.parentElement;
+//            itemList.removeChild(li);
+           
+//        }
+       
+//    }
+    
+// }
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let button = document.getElementById('button');
+// button.addEventListener('mouseup',clickBtn);
+
+
+
+
+
+
+
+// function clickBtn (e) {
+//     console.log('Even Type :'+e.type);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let button = document.getElementById('button').addEventListener('click',clickBtn);
+
+
+
+// function clickBtn(e) {
+//    console.log(e.target.id);
+//    console.log(e.target.className);
+//    console.log(e.target.type);
+//    console.log(e.clientX);
+
+
+   
+// }
+// function clickBtn() {
+//     document.getElementById("header1    ").style.backgroundColor = 'red'; 
+//    document.getElementById("title").textContent = 'Change'; 
+// }
+// var newDev = document.createElement('div');
+
+//    newDev.className = 'Hello';
+
+// console.log(newDev);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     var even = document.querySelectorAll(".list-name li:nth-child(even)");
+//     var odd = document.querySelectorAll(".list-name li:nth-child(odd)");
+
+// // console.log(text);
+
+// function rehman(){
+
+// for (let index = 0; index < even.length; index++) {
+    
+//     even[index].style.color = "red";
+//     odd[index].style.color = "blue";
+    
+// }
+// }
+
+// function rehman() {
+//     // alert();
+   
+   
+    
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
